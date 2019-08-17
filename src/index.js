f870e0b471e04cf6e645cddde7ace03d28695838
Jean-Marc MONTOUT <mjean_marc@hotmail.com>
2019-08-18 00:38:49 +0200

    Rewrite increment() and decrement()

--- src/index.js
+++ src/index.js
@@ -22,28 +22,32 @@ function Controls(props) {
 }
 
 function Session({ timer }) {
-  function format() {
-    let minutes = Math.floor(timer / 60);
+  const [timeLeft, setTimeLeft] = useState(() => {
+    console.log(timer);
+    return timer + ":00";
+  });
+  function format(time) {
+    let minutes = Math.floor(time / 60);
     minutes = minutes < 10 ? "0" + minutes : minutes;
-    let seconds = timer - minutes * 60;
+    let seconds = time - minutes * 60;
     seconds = seconds < 10 ? "0" + seconds : seconds;
     return minutes + ":" + seconds;
   }
 
-  let timeLeft = format();
+  //let timeLeft = format();
 
   return (
     <div className="session">
       <div className="session-wrapper">
         <div id="timer-label">Session</div>
-        <div id="time-left">{timeLeft}</div>
+        <div id="time-left">{timer}</div>
       </div>
     </div>
   );
 }
 
 function DisplayLengthSettings(props) {
-  const [value, setValue] = useState(props.timer);
+  // const [value, setValue] = useState(props.timer);
   const settingName = props.settingName;
   const settingLengthWrapper = `${settingName.toLowerCase()}-length-wrapper`;
   const settingLabel = `${settingName.toLowerCase()}-label`;
@@ -55,11 +59,11 @@ function DisplayLengthSettings(props) {
     <div className={settingLengthWrapper}>
       <div id={settingLabel}>{settingName} Length</div>
       <div className="settings-wrapper">
-        <button id={settingDecrement} onClick={() => setValue(value - 1)}>
+        <button id={settingDecrement} onClick={props.decrementSetting}>
           <i className="fas fa-arrow-down" />
         </button>
-        <div id={settingLength}>{value}</div>
-        <button id={settingIncrement} onClick={() => setValue(value + 1)}>
+        <div id={settingLength}>{props.timer}</div>
+        <button id={settingIncrement} onClick={props.incrementSetting}>
           <i className="fas fa-arrow-up" />
         </button>
       </div>
@@ -74,16 +78,42 @@ function App() {
   const [sessionLength, setSessionLength] = useState(defaultSessionLength);
   const [breakLength, setBreakLength] = useState(defaultBreakLength);
 
-  const [length, setLength] = useState(1500);
+  //const [length, setLength] = useState(1500);
+
+  function decrementSession() {
+    setSessionLength(sessionLength - 1);
+  }
+
+  function incrementSession() {
+    setSessionLength(sessionLength + 1);
+  }
+
+  function decrementBreak() {
+    setBreakLength(breakLength - 1);
+  }
+
+  function incrementBreak() {
+    setBreakLength(breakLength + 1);
+  }
 
   return (
     <div className="App">
       <h1>{projectName}</h1>
       <Controls />
-      <Session timer={length} />
+      <Session timer={sessionLength} />
       <div className="settings">
-        <DisplayLengthSettings settingName="Session" timer={sessionLength} />
-        <DisplayLengthSettings settingName="Break" timer={breakLength} />
+        <DisplayLengthSettings
+          settingName="Session"
+          timer={sessionLength}
+          decrementSetting={decrementSession}
+          incrementSetting={incrementSession}
+        />
+        <DisplayLengthSettings
+          settingName="Break"
+          timer={breakLength}
+          decrementSetting={decrementBreak}
+          incrementSetting={incrementBreak}
+        />
       </div>
       <footer>
         <Signature />
